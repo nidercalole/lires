e4f53205be206112adfe3f10b5f6e927916dfeb2
einswyldesacin <133804170+Nica3720@users.noreply.github.com>
2024-05-31 14:25:20 +0000

    Beginn Unterer Webseitenteil

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,15 @@
 const abc = document.getElementById('test')
+
+let ended = false; // will change to true after the animation finishes
+
+    ListBut.onclick = function() {
+
+      listArea.addEventListener('transitionend', function() {
+        if (!ended) { // check to show the message only once
+          ended = true;
+          alert('Done!');
+        }
+      });
+
+      listArea.classList.add('growing');
+    }
