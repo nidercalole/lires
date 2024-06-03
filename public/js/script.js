05a50c29bbc082ff0d55cd8aff596960132869df
einswyldesacin <133804170+Nica3720@users.noreply.github.com>
2024-06-03 16:32:48 +0000

    kleine süße tooltips und Filter (aber noch nicht fertig und ohne funktion bisher, natürlich :ino:)

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', (event) => {
   let ended = false;
   const ListBut = document.getElementById('ListBut');
@@ -13,3 +12,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
       ended = !ended; // ich weiß nicht, wie chatgpt das gemacht hat, aber es funktioniert hervorragend, also bleibt nun dieser code cF
   } //vorallem funktioniert es aber jetzt sogar im richtigen dokument :D
 });
+
+function filBut(){
+    const filOptionPage = document.getElementById('filOptionPage');
+    if (filOptionPage.style.display === 'none' || filOptionPage.style.display === '') {
+        filOptionPage.style.display = 'grid';
+    } else{
+        filOptionPage.style.display = 'none';
+    }
+}
