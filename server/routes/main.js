949248e5ab0f950c7b7b5f7ee3c6a4feaa4e3d22
derlole <122916573+derlole@users.noreply.github.com>
2024-06-04 06:08:51 +0000

    login beginn

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Lires' });
+    res.render('index', { title: 'Lires', usrnm: 'Nicht eingeloggt' });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login', { title: 'Lires', usrnm: 'Nicht eingeloggt' });
 });
 
 module.exports = router;
