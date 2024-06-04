b0e80dd27925d5197b70e309e080a9b5acf45fe8
derlole <122916573+derlole@users.noreply.github.com>
2024-06-04 20:22:03 +0000

    login

--- server/routes/main.js
+++ server/routes/main.js
@@ -16,20 +16,28 @@ function insertUsr(data) {
         });
 }
 
-function findUsr(query) {
-    Usrnm.find(query)
-        .then((usrnms) => {
-            console.log('Usrnm found:', usrnms);
-        })
-        .catch((err) => {
-            console.log('Error:', err);
-        })
-}
-
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.get('/', (req, res) => {
-    res.render('index', { title: 'Lires', usrnm: req.query.usrnm });
+router.get('/', async(req, res) => {
+    console.log('Usrnm:', req.query.usrid);
+    if(req.query.usrnm === undefined) {
+        return res.redirect('/login');
+    }else{
+        try{
+            const query = { usrid: req.query.usrid };
+            const existingUser = await Usrnm.findOne(query).exec();
+            console.log('Existing user:', existingUser);
+            if(existingUser){
+                const username = existingUser.usrnm;
+                return res.render('index', { title: 'Lires', usrnm: username });
+            }else{
+                return res.redirect('/login');
+            }
+        }catch(err){
+            console.error(err);
+            return res.status(500).send('Fehler bei der Verarbeitung der Anfrage.');
+        }
+    }
 });
 
 router.get('/login', (req, res) => {
@@ -43,60 +51,66 @@ router.get('/addres', (req, res) => {
 router.post('/register', async (req, res) => {
     const username = req.body.username;
     const permstring = req.body.permstring;
-    const permstringPermision = 's4l4m4nderBrei'
-    if(permstring === permstringPermision){
+    const permstringPermision = 's4l4m4nderBrei';
+
+    if (permstring === permstringPermision) {
         console.log('Permission granted');
         try {
-            const existingUser =  await Usrnm.findOne({usrnm: username}).exec();
+            const existingUser = await Usrnm.findOne({ usrnm: username }).exec();
             console.log('Existing user:', existingUser);
-            if(existingUser){
+            if (existingUser) {
                 console.log('User already exists');
-                res.redirect('/login');
-            }else{
-                insertUsr({usrnm: username});
-                res.redirect('/verify/?usrnm=' + username);
+                return res.redirect('/login');
+            } else {
+                await insertUsr({ usrnm: username });
+                return res.redirect('/verify/?usrnm=' + username);
             }
-        }catch (err) {
+        } catch (err) {
             console.error(err);
-            res.status(500).send('Fehler bei der Verarbeitung der Anfrage.');
+            return res.status(500).send('Fehler bei der Verarbeitung der Anfrage.');
         }
-        
-    }else{
+    } else {
         console.log('Permission denied');
-        res.redirect('/login');
+        return res.redirect('/login');
     }
-    console.log('Username:', username);
-
-    res.redirect('/login');
 });
+
 
 router.get('/verify', async (req, res) => {
     try {
         const username = req.query.usrnm;
-        const query = {usrnm: username};
+        const query = { usrnm: username };
         const existingUser = await Usrnm.findOne(query).exec();
-        const id = existingUser.usrid;
-        res.redirect('/?usrid=' + id + '&usrnm=' + username);
-    }catch (err) {
+        console.log('Existing user:', existingUser);
+        if (existingUser) {
+            const id = existingUser.usrid;
+            return res.redirect('/?usrid=' + id + '&usrnm=' + username);
+        } else {
+            return res.status(404).send('Benutzer nicht gefunden.');
+        }
+    } catch (err) {
         console.error(err);
-        res.status(500).send('Fehler bei der Verarbeitung der Anfrage.');
+        return res.status(500).send('Fehler bei der Verarbeitung der Anfrage.');
     }
 });
 
-router.post('/anmld' , async (req, res) => {
+
+router.post('/anmld', async (req, res) => {
     try {
         const username = req.body.username;
-        const query = {usrnm: username};
+        const query = { usrnm: username };
         const existingUser = await Usrnm.findOne(query).exec();
-        if(!existingUser){
+        
+        if (!existingUser) {
             console.log('User not found');
-            res.redirect('/login');
+            return res.redirect('/login');
         }
+        
         const id = existingUser.usrid;
-        res.redirect('/?usrid=' + id + '&usrnm=' + username);
-    }catch (err) {
+        return res.redirect('/?usrid=' + id + '&usrnm=' + username);
+    } catch (err) {
         console.error(err);
-        res.status(500).send('Fehler bei der Verarbeitung der Anfrage.');
+        return res.status(500).send('Fehler bei der Verarbeitung der Anfrage.');
     }
 });
 
