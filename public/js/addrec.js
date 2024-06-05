7c99022782107235e541bbda691dde24b6652cad
derlole <122916573+derlole@users.noreply.github.com>
2024-06-05 17:51:12 +0000

    textcontent

--- public/js/addrec.js
+++ public/js/addrec.js
@@ -23,8 +23,8 @@ function addTableRow() {
   var row = table.insertRow(-1);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
-  cell1.innerHTML = "NEW CELL1";
-  cell2.innerHTML = "NEW CELL2";
+  cell1.textContent = "NEW CELL1";
+  cell2.textContent = "NEW CELL2";
 
   cell1.id = "resing_" + counter;
   cell2.id = "resingamount_" + counter;
