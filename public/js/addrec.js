6cf655f5a487704d48226b0cb3a0d5d9e790c58c
derlole <122916573+derlole@users.noreply.github.com>
2024-06-05 18:24:45 +0000

    addrec

--- public/js/addrec.js
+++ public/js/addrec.js
@@ -3,12 +3,14 @@
 
 let counter = 0;
 function addTableRow() {
+  const tableInp11 = document.getElementById("tableInp11").value;
+  const tableInp12 = document.getElementById("tableInp12").value;
   var table = document.getElementById("table1");
   var row = table.insertRow(-1);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
-  cell1.textContent = "NEW CELL1";
-  cell2.textContent = "NEW CELL2";
+  cell1.textContent = tableInp11
+  cell2.textContent = tableInp12
 
   cell1.id = "resing_" + counter;
   cell2.id = "resingamount_" + counter;
@@ -16,7 +18,7 @@ function addTableRow() {
   counter++;
 }
 
-var input = document.getElementById("tableInp1");
+var input = document.getElementById("backfisch");
 input.addEventListener("keypress", function(event) {
   if (event.key === "Enter") {
     event.preventDefault();
