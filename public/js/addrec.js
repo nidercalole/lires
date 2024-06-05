28cd84f0a4e2f887a00bda0950f1b8f20caa015d
einswyldesacin <133804170+Nica3720@users.noreply.github.com>
2024-06-05 13:04:37 +0000

    Js ist cursed aber der Rest wird immer besser :D

---
+++ public/js/addrec.js
@@ -0,0 +1,13 @@
+var input = document.getElementById("tableInp1");
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    function addTableRow() {
+        var table = document.getElementById("table1");
+        var row = table.insertRow(0);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        cell1.innerHTML = "NEW CELL1";
+        cell2.innerHTML = "NEW CELL2";
+      }
+  }
+});
