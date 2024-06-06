9d136b1d2f77a08de729595296e2232426540312
derlole <122916573+derlole@users.noreply.github.com>
2024-06-06 20:12:33 +0000

    loginerrors

--- public/js/loginerr.js
+++ public/js/loginerr.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const centerdiv = document.getElementById('centerDiv');
+    const reger = document.getElementById('regerror');
+    const logerr = document.getElementById('loginerror');
+
+
     const url = new URLSearchParams(window.location.search);
     const islogin = url.get('islogin');
     const message = url.get('message');
-    console.log(islogin, message);
+
+    centerdiv.style.display = 'none';
+    reger.style.display = 'none';
+    logerr.style.display = 'none';
+
     if (islogin === 'false') {
-        document.getElementById('regerror').textContent = message;
+        centerdiv.style.display = 'block';
+        reger.style.display = 'block';
+        reger.textContent = message;
     } else if (islogin === 'true') {
-        document.getElementById('loginerror').textContent = message;
+        centerdiv.style.display = 'block';
+        logerr.style.display = 'block';
+        logerr.textContent = message;
     }
-    });
+});
