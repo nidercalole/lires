ddf9bbe98347ec79aac4eccc9ab8dbc9b8e69f86
einswyldesacin <133804170+Nica3720@users.noreply.github.com>
2024-06-11 21:17:08 +0000

    addrec auf mobile lebt, aber die mechanik fehlt noch komplett Und teile sind zu breit -.-

--- server/routes/mobile.js
+++ server/routes/mobile.js
@@ -6,17 +6,21 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
-    if(!req.useragent.isMobile) {
+    if (!req.useragent.isMobile) {
         return res.redirect('/');
     }
     res.render('mobile/index', { title: 'Lires', usrnm: req.query.usrnm });
 });
 
 router.get('/lists', (req, res) => {
-    res.render('mobile/lists', { title: 'Lires', usrnm: req.query.usrnm});
-  });
+    res.render('mobile/lists', { title: 'Lires', usrnm: req.query.usrnm });
+});
 
 router.get('/notsupported', (req, res) => {
     res.render('mobile/notsupported', { title: 'Lires', usrnm: req.query.usrnm });
+});
+
+router.get('/addrec', (req, res) => {
+    res.render('mobile/addrec', { title: 'Lires', usrnm: req.query.usrnm });
 });
 module.exports = router;
