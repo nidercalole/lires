9a6af3cdcffa91b9e57d31016fadeac910df165e
derlole <122916573+derlole@users.noreply.github.com>
2025-05-22 12:15:48 +0000

    subrec schema concept

---
+++ server/models/subrec.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose')
+
+const subrecSchema = new mongoose.Schema({
+    recname: {
+        type: String,
+        required: true
+    },
+    parentrecid: {
+        type: String,
+        required: true
+    },
+    ts: {
+        type: Date,
+        default: Date.now
+    },
+    recid: {
+        type: String,
+        unique: true,
+        default: Math.random().toString(36).substring(2)
+    },
+    user:{
+        type: Array,
+        default: []
+    },
+    expense:{
+        type: Number,
+        required: true
+    },
+    duration:{
+        type: Number,
+        required: true
+    },
+    description:{
+        type: String,
+        default: ''
+    },
+    countfor:{
+        type: Array,
+        default: []
+    },
+    kindodish:{
+        type: Array,
+        required: true
+    },
+    labels:{
+        type: Array,
+        default: []
+    },
+    ingredients:{
+        type: Array,
+        default: []
+    },
+    directions:{
+        type: Array,
+        default: []
+    },
+    extra:{
+        type: Array,
+        default: []
+    }
+})
+
+module.exports = mongoose.model('Subrec', subrecSchema)
