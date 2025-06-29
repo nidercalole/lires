610d02d591e3d65b7e997e1e8bb4080a121494b0
derlole <122916573+derlole@users.noreply.github.com>
2025-06-29 18:30:37 +0000

    ich sowas von aal

--- public/js/scroll_allrecs.js
+++ public/js/scroll_allrecs.js
@@ -7,3 +7,16 @@ document.querySelectorAll('.letter-bar span').forEach(span => {
         }
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.tablePos h2').forEach(function(h2) {
+        const table = h2.nextElementSibling;
+        if (table && table.tagName === 'TABLE') {
+            if (table.querySelectorAll('tr').length === 0) {
+                h2.style.display = 'none';
+                table.style.display = 'none';
+            }
+        }
+    });
+});
